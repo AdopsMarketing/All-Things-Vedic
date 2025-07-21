00140958d3ce42119caf5484271a0bbe162866fc
AdopsMarketing <adops@orangecarrotmedia.com>
2025-07-21 20:28:48 +0530

    Fixed GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-import Head from 'next/head';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
   title: "All Things Vedic",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,24 +25,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M3XQF4BS');
-            `,
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-M3XQF4BS');
+              `,
           }}
-        ></script>
-      </Head>
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3XQF4BS"
-          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-M3XQF4BS`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
         {children}
       </body>
     </html>
